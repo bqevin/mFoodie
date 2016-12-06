f855ea6d0c8d82505601707a4290227e799f0fb4
Kevin Barasa <kevin.barasa001@gmail.com>
2016-12-06 19:28:32 +0300

    Added Login and Register API with SQLite support for offline sessions storage

---
+++ app/src/main/java/com/projects/kevinbarassa/thefoorapp/AppConfig.java
@@ -0,0 +1,12 @@
+package com.projects.kevinbarassa.thefoorapp;
+
+/**
+ * Created by Kevin Barassa on 06-Dec-16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://agrigender.net/stylus/users/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://agrigender.net/stylus/users/register.php";
+}
